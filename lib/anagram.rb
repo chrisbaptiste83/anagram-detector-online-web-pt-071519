530e13b34f1f944bca6cfba89b6ffbfcbb8e6088
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-11 02:52:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,6 +3,7 @@ class Anagram
   def initialize(word) 
     @word = word 
   end 
+  
 def match(word_array) 
   word_array.select do |new_word| new_word.split("").sort == @word.split("").sort 
 end 
