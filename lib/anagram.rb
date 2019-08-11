0b32bad91efec1f6f202b358b23ee1feb74c1ca4
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-11 02:50:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,8 +4,8 @@ class Anagram
     @word = word 
   end 
 def match(word_array) 
-  word_array.each do |new_word| if new_word.split("").sort == @word.split("").sort 
-  true 
+  word_array.each do |new_word| new_word.split("").sort == @word.split("").sort 
+  puts true 
 else 
   false
 end 
